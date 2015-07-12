8af325639ceb3a9e5dab0e4e38a070b5612062f3
brayanNunez <brayannr22@gmail.com>
2015-07-12 01:19:46 -0600

    paso2

---
+++ application/views/cotizar/paso2.php
@@ -0,0 +1,121 @@
+<!-- START CONTENT -->
+ <section id="content">
+
+    <!--start container-->
+        <div id="breadcrumbs-wrapper" class=" grey lighten-3">
+          <div class="container">
+            <div class="row">
+              <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title"><?=label('tituloCotizacion');?></a></h5>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--breadcrumbs end-->
+
+
+
+
+    <div class="container">
+        <div id="chart-dashboard">
+            <div class="row">
+                <div class="col s12 m12 l12">
+
+                    <div id="submit-button" class="section">
+                        <div class="row">
+                            
+                            <div class="col s12 m12 l12">
+                              <div class="card" id="card-paso1">
+
+
+                                      <div id="hoverable-table">
+                                          <h4 class="header">Hoverable Table</h4>
+                                          <div class="row">
+                                            <!-- <div class="col s12 m4 l3">
+                                              <p>Add <code class=" language-markup">class="hoverable"</code> to the table tag for a hoverable table</p>
+                                            </div> -->
+                                            <div class="col s12 m12 l12">
+                                              <table class="hoverable">
+                                                <thead>
+                                                  <tr>
+                                                    <th data-field="id"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>Nombre</th>
+                                                    <th data-field="name"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>Descripción</th>
+                                                    <th data-field="price"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>Imagen</th>
+                                                    <th data-field="price"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>Precio unitario</th>
+                                                    <th data-field="price"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>cantidad</th>
+                                                    <th data-field="price"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>IV</th>
+                                                    <th data-field="price"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>Unidad</th>
+                                                    <th data-field="price"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>Subtotal</th>
+                                                    <th data-field="price"><input type="checkbox" id="test6"><label class="ver" for="test6"></label>Opciones</th>
+                                                  </tr>
+                                                </thead>
+                                                <tbody>
+                                                  <tr>
+                                                  
+                                                    
+                                                 
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input value="Hola mundo" type="text" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test7"><label class="ver" for="test7"></label><input type="text" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test8"><label class="ver" for="test8"></label></td>
+                                                    <td> <input type="checkbox" id="test9"><label class="ver" for="test9"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test10"><label class="ver" for="test10"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test11"><label class="ver" for="test11"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test12"><label class="ver" for="test12"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test13"><label class="ver" for="test13"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test14"><label class="ver" for="test14"></label><button></button></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input value="Hola mundo" type="text" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="text" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><button></button></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input value="Hola mundo" type="text" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="text" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><button></button></td>
+                                                  </tr>
+                                                  <tr>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input value="Hola mundo" type="text" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="text" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><input type="number" name="usrname"></td>
+                                                    <td> <input type="checkbox" id="test6"><label class="ver" for="test6"></label><button></button></td>
+                                                  </tr>
+                                                </tbody>
+                                              </table>
+                                            </div>
+                                          </div>
+                                        </div>
+
+
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!--end container-->
+</section>
+<!-- END CONTENT-->
+
