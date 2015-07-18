af03e014fab97291e24168380a3746969fb91a36
brayanNunez <brayannr22@gmail.com>
2015-07-18 10:55:06 -0600

    diseno

--- application/views/cotizar/paso3.php
+++ application/views/cotizar/paso3.php
@@ -65,48 +65,53 @@
                                     <a href="#modalCuerpo" class="modal-trigger btn-floating activator btn-move-up waves-effect waves-light darken-2 right">
                                     <i class="mdi-editor-mode-edit"></i>
                                     </a>
-                                    <div id="detalle">
-                                       <div id="datallesCotizacion">
-                                          <table style="width:100%">
-                                             <tr>
-                                                <th>Nombre</th>
-                                                <th>Precio</th>
-                                                <th>Cantidad</th>
-                                                <th>Imagen</th>
-                                                <th>Sub-total
-                                                   <a onclick="darclick(2);" class="btn-floating activator btn-move-up waves-effect waves-light darken-2 right">
-                                                   <i class="mdi-editor-mode-edit"></i>
+                                    
+                                    
+                                       <div id="detalle">
+
+                                          <div id="datallesCotizacion">
+                                             <table style="width:100%">
+
+                                                <tr>
+                                                   <th>Nombre</th>
+                                                   <th>Precio</th>
+                                                   <th>Cantidad</th>
+                                                   <th>Imagen</th>
+                                                   <th>Sub-total
+                                                   <a id="editarLineas" onclick="darclick(2);" class="btn-floating activator btn-move-up waves-effect waves-light darken-2 right">
+                                                      <i class="mdi-editor-mode-edit"></i>
                                                    </a>
-                                                </th>
-                                             </tr>
-                                             <tr>
-                                                <td>Almuerzo</td>
-                                                <td>$6</td>
-                                                <td>20</td>
-                                                <td><img src="<?=base_url()?>assets/dashboard/images/almuerzo.jpg"></td>
-                                                <td>$120</td>
-                                             </tr>
-                                             <tr>
-                                                <td>Fresco</td>
-                                                <td>$1</td>
-                                                <td>20</td>
+                                                   </th>
+                                                </tr>
+                                                <tr>
+                                                   <td>Almuerzo</td>
+                                                   <td>$6</td>
+                                                   <td>20</td>
+                                                   <td><img src="<?=base_url()?>assets/dashboard/images/almuerzo.jpg"></td>
+                                                   <td>$120</td>
+                                                </tr>
+                                                <tr>
+                                                   <td>Fresco</td>
+                                                   <td>$1</td>
+                                                   <td>20</td>
+                                                   <td></td>
+                                                   <td>$20</td>
+                                                </tr>
+                                                <td>Música</td>
+                                                <td>$30</td>
                                                 <td></td>
-                                                <td>$20</td>
-                                             </tr>
-                                             <td>Música</td>
-                                             <td>$30</td>
-                                             <td></td>
-                                             <td><img src="<?=base_url()?>assets/dashboard/images/musica.jpg"></td>
-                                             <td>$30</td>
-                                             </tr>
-                                          </table>
+                                                <td><img src="<?=base_url()?>assets/dashboard/images/musica.jpg"></td>
+                                                <td>$30</td>
+                                                </tr>
+                                             </table>
+                                          </div>
+                                          <div id="resultadoCotizacion">
+                                             <p>Impuesto: 13%</p>
+                                             <p>Descuento: 10%</p>
+                                             <p>Total: $170</p>
+                                          </div>
                                        </div>
-                                       <div id="resultadoCotizacion">
-                                          <p>Impuesto: 13%</p>
-                                          <p>Descuento: 10%</p>
-                                          <p>Total: $170</p>
-                                       </div>
-                                    </div>
+
                                     <div class="barra-horizontal" id="barra2">
                                     </div>
                                     <a href="#modalInformacion" class="modal-trigger btn-floating activator btn-move-up waves-effect waves-light darken-2 right">
