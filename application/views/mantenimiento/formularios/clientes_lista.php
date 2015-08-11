44d915d5f0716f491c149f6832a038fdbf675823
sebas07 <josesrb10@outlook.com>
2015-08-11 16:17:04 -0600

    Registro y busqueda avanzada de clientes

--- application/views/mantenimiento/formularios/clientes_lista.php
+++ application/views/mantenimiento/formularios/clientes_lista.php
@@ -29,6 +29,7 @@
                                                     <div class="agregar_nuevo">
                                                         <a href="<?=base_url()?>clientes/agregar" class="btn btn-default"><?=label('agregar_nuevo');?></a>
                                                     </div>
+                                                    <a id="busqueda-avanzada-cliente" href="#busquedaAvanzada" class="modal-trigger">Busqueda avanzada</a>
                                                     <thead>
                                                         <tr>
                                                             <th><?=label('Cliente_tablaCodigo');?></th>
@@ -165,6 +166,185 @@
     </div>
     <div class="modal-content">
         <p><?=label('confirmarEliminarCliente');?></p>
+    </div>
+    <div class="modal-footer black-text">
+        <a href="#" class="waves-effect waves-red btn-flat modal-action modal-close"><?=label('aceptar');?></a>
+    </div>
+</div>
+<div id="busquedaAvanzada" class="modal">
+    <div class="modal-header">
+        <p><?=label('nombreSistema');?></p>
+        <a class="modal-action modal-close cerrar-modal"><i class="mdi-content-clear"></i></a>
+    </div>
+    <div class="modal-content">
+        <div id="formGeneral" class="section">
+            <div class="row">
+                <div class="input-field col s12 m3 l3">
+                    <div class="input-field col s12">
+                        <input id="busqueda-fecha-desde" type="date">
+                         <label id="fecha-desde" for="busqueda-fecha-desde" class="">Desde: </label>
+                    </div>
+                </div>
+                <div class="input-field col s12 m3 l3">
+                    <div class="input-field col s12">
+                        <input id="busqueda-fecha-hasta" type="date">
+                        <label id="fecha-hasta" for="busqueda-fecha-hasta" class="">Hasta: </label>
+                    </div>
+                </div>
+
+                <div class="input-field col s12 m6 l6">
+                    <select class="input-field col s12">
+                        <!--	                                 <option value="" disabled selected>Estado</option>-->
+                        <option value="1" selected>Todos</option>
+                        <option value="2">Enviada</option>
+                        <option value="3">Finalizada</option>
+                        <option value="4">Rechazada</option>
+                    </select>
+                    <label>Estado de la cotización</label>
+                </div>
+                <div class="input-field col s12 m4 l4">
+                    <select class="input-field col s12">
+                        <!--	                                 <option value="" disabled selected>Cliente</option>-->
+                        <option value="1" selected>Todos</option>
+                        <option value="2">Juan Alfaro Alfaro</option>
+                        <option value="3">Diego Rojas</option>
+                    </select>
+                    <label>Clientes</label>
+                </div>
+                <div class="input-field col s12 m4 l4">
+                    <select class="input-field col s12">
+                        <!--	                                 <option value="" disabled selected>Empleados</option>-->
+                        <option value="1" selected>Todos</option>
+                        <option value="2">Juan Carlos Porras</option>
+                        <option value="3">Ana Bolaños Rojas</option>
+                    </select>
+                    <label>Vendedores</label>
+                </div>
+                <div class="input-field col s12 m4 l4">
+                    <select id="reporte-cliente" class="input-field col s12">
+                        <!--                                     <option value="" disabled selected>Outsourcing</option>-->
+                        <option value="1" selected>Todos</option>
+                        <option value="2">Transportes Rojas</option>
+                        <option value="3">Música en vivo</option>
+                    </select>
+                    <label for="reporte-cliente">Proveedores</label>
+                </div>
+                <div class="input-field col s12 m6 l6">
+                    <label><?=label('formCliente_gustos_preferencias');?></label>
+                    <br />
+                    <br />
+                    <table class="table striped">
+                        <thead>
+                        <tr>
+                            <th><?=label('formCliente_gustos');?></th>
+                            <th><?=label('formCliente_estado');?></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Preferencia 1</td>
+                            <td>
+                                <div class="switch">
+                                    <label style="position: relative">
+                                        <?=label('off');?>
+                                        <input type="checkbox">
+                                        <span class="lever"></span>
+                                        <?=label('on');?>
+                                    </label>
+                                </div>
+                                <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Preferencia 2</td>
+                            <td>
+                                <div class="switch">
+                                    <label style="position: relative">
+                                        <?=label('off');?>
+                                        <input type="checkbox">
+                                        <span class="lever"></span>
+                                        <?=label('on');?>
+                                    </label>
+                                </div>
+                                <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Preferencia 3</td>
+                            <td>
+                                <div class="switch">
+                                    <label style="position: relative">
+                                        <?=label('off');?>
+                                        <input type="checkbox">
+                                        <span class="lever"></span>
+                                        <?=label('on');?>
+                                    </label>
+                                </div>
+                                <br />
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="input-field col s12 m6 l6">
+                    <label><?=label('formCliente_mediosContacto');?></label>
+                    <br />
+                    <br />
+                    <table class="table striped">
+                        <thead>
+                        <tr>
+                            <th><?=label('formCliente_medio');?></th>
+                            <th><?=label('formCliente_estadoMedio');?></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Medio 1</td>
+                            <td>
+                                <div class="switch">
+                                    <label style="position: relative">
+                                        <?=label('off');?>
+                                        <input type="checkbox">
+                                        <span class="lever"></span>
+                                        <?=label('on');?>
+                                    </label>
+                                </div>
+                                <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Medio 2</td>
+                            <td>
+                                <div class="switch">
+                                    <label style="position: relative">
+                                        <?=label('off');?>
+                                        <input type="checkbox">
+                                        <span class="lever"></span>
+                                        <?=label('on');?>
+                                    </label>
+                                </div>
+                                <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Medio 3</td>
+                            <td>
+                                <div class="switch">
+                                    <label style="position: relative">
+                                        <?=label('off');?>
+                                        <input type="checkbox">
+                                        <span class="lever"></span>
+                                        <?=label('on');?>
+                                    </label>
+                                </div>
+                                <br />
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="modal-footer black-text">
         <a href="#" class="waves-effect waves-red btn-flat modal-action modal-close"><?=label('aceptar');?></a>
--- application/views/mantenimiento/formularios/registro.php
+++ application/views/mantenimiento/formularios/registro.php
@@ -25,45 +25,63 @@
                                 <form class="col s12">
                                     <div class="row">
                                         <div class="input-field col s12">
+                                            <input id="registro_nombreEmpresa" class="campo-registro" type="text"
+                                                   placeholder="Escriba el nombre de su compañía">
+<!--                                            <label for="registro_nombreEmpresa" class="active">--><?//=label('formRegistro_nombre');?><!--</label>-->
+                                        </div>
+                                        <div class="input-field col s12">
                                             <input id="registro_nombre" class="campo-registro" type="text"
                                                    placeholder="Escriba su nombre completo">
-                                            <label for="registro_nombre" class="active"><?=label('formRegistro_nombre');?></label>
+<!--                                            <label for="registro_nombre" class="active">--><?//=label('formRegistro_nombre');?><!--</label>-->
                                         </div>
                                         <div class="input-field col s12">
                                             <input id="registro_correo" class="campo-registro" type="email"
                                                 placeholder="Escriba su correo electrónico">
-                                            <label for="registro_correo" class="active"><?=label('formRegistro_correo');?></label>
+<!--                                            <label for="registro_correo" class="active">--><?//=label('formRegistro_correo');?><!--</label>-->
                                         </div>
                                         <div class="input-field col s12">
                                             <input id="registro_telefono" class="campo-registro" type="text"
                                                 placeholder="Escriba su número de teléfono">
-                                            <label for="registro_telefono" class="active"><?=label('formRegistro_telefono');?></label>
+<!--                                            <label for="registro_telefono" class="active">--><?//=label('formRegistro_telefono');?><!--</label>-->
                                         </div>
                                         <div class="input-field col s12">
                                             <input id="registro_sitioWeb" class="campo-registro" type="text"
                                                 placeholder="Escriba su sitio web">
-                                            <label for="registro_sitioWeb" class="active"><?=label('formRegistro_sitioWeb');?></label>
+<!--                                            <label for="registro_sitioWeb" class="active">--><?//=label('formRegistro_sitioWeb');?><!--</label>-->
                                         </div>
                                         <div class="input-field col s12">
                                             <input id="registro_nombreUsuario" class="campo-registro" type="text"
                                                 placeholder="Escriba su nombre de usuario">
-                                            <label for="registro_nombreUsuario" class="active"><?=label('formRegistro_nombreUsuario');?></label>
+<!--                                            <label for="registro_nombreUsuario" class="active">--><?//=label('formRegistro_nombreUsuario');?><!--</label>-->
                                         </div>
                                         <div class="input-field col s12">
                                             <input id="registro_contrasena" class="campo-registro" type="password"
                                                 placeholder="Escriba su contraseña">
-                                            <label for="registro_contrasena" class="active"><?=label('formRegistro_contrasena');?></label>
+<!--                                            <label for="registro_contrasena" class="active">--><?//=label('formRegistro_contrasena');?><!--</label>-->
                                         </div>
                                         <div class="input-field col s12">
                                             <input id="registro_contrasenaConfirm" class="campo-registro" type="password"
                                                 placeholder="Confirme su contraseña">
-                                            <label for="registro_contrasenaConfirm" class="active"><?=label('formRegistro_contrasenaConfirm');?></label>
+<!--                                            <label for="registro_contrasenaConfirm" class="active">--><?//=label('formRegistro_contrasenaConfirm');?><!--</label>-->
+                                        </div>
+                                        <div class="input-field col s12 country-select">
+                                            <select>
+                                                <option class="selected-option" selected disabled>Seleccione el país al que pertenece</option>
+                                                <option value="1">Costa Rica</option>
+                                                <option value="2">Brasil</option>
+                                                <option value="3">USA</option>
+                                                <option value="4">Colombia</option>
+                                                <option value="5">Chile</option>
+                                            </select>
+<!--                                            <input id="registro_country" class="campo-registro" type="password" placeholder="Seleccione el país al que pertenece">-->
+<!--                                            <label for="registro_country" class="active">--><?//=label('formRegistro_contrasenaConfirm');?><!--</label>-->
                                         </div>
                                         <div class="file-field col s12">
                                             <br />
-                                            <label for="registro_fotografia"><?=label('formRegistro_fotografia');?></label>
+<!--                                            <label for="registro_fotografia">--><?//=label('formRegistro_fotografia');?><!--</label>-->
                                             <div class="file-field input-field col s12">
-                                                <input class="file-path validate campo-registro" type="text"/>
+                                                <input class="file-path validate campo-registro" type="text"
+                                                    placeholder="Seleccione una imagen"/>
                                                 <div class="btn" data-toggle="tooltip" title="<?=label('tooltip_examinar')?>"
                                                     style="border-radius: 0px !important; min-width: 13%;">
                                                     <span><i class="mdi-action-search"></i></span>
@@ -72,7 +90,8 @@
                                             </div>
                                         </div>
                                         <div class="input-field col s12 campo-captcha">
-                                            <input type="text" id="defaultReal" name="defaultReal" class="campo-registro">
+                                            <input type="text" id="defaultReal" name="defaultReal" class="campo-registro"
+                                                placeholder="Ingrese el captcha">
                                         </div>
                                         <div class="input-field col s12 envio-formulario">
                                             <button class="btn btn-filled registrar" type="submit" name="action"><?=label('formRegistro_crearPerfil');?>
