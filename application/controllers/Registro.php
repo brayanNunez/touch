8b2259111bb0aff9bc82b93854d239ebb9172a31
Sebastian R <josesrb10@outlook.com>
2015-10-08 22:45:52 -0600

    Registro

--- application/controllers/Registro.php
+++ application/controllers/Registro.php
@@ -20,60 +20,75 @@ class Registro extends CI_Controller
 
     public function registrar()
     {
-        $tipoRegistro = $this->input->post('registro_tipo');
-        $data = array();
-        echo 'El tipo de registro es '.$tipoRegistro.'<br/>';
-        if($tipoRegistro == 1) {
-            $data['datos'] = array(
-                'cedula' => $this->input->post('registro_cedulaTrabajador'),
-                'nombre' => $this->input->post('registro_nombreEmpresaTrabajador'),
-                'idMoneda' => 1
-            );
-            $data['direccion'] = array(
-                'pais' => $this->input->post('registro_paisTrabajador'),
-                'provincia' => $this->input->post('registro_provinciaTrabajador'),
-                'canton' => $this->input->post('registro_cantonTrabajador'),
-                'domicilio' => $this->input->post('registro_domicilioTrabajador')
-            );
-            $data['usuario'] = array(
-                'primerApellido' => $this->input->post('registro_primerApellidoTrabajador'),
-                'segundoApellido' => $this->input->post('registro_segundoApellidoTrabajador'),
-                'nombre' => $this->input->post('registro_nombreTrabajador'),
-                'correo' => $this->input->post('registro_correoTrabajador'),
-                'contrasena' => $this->input->post('registro_contrasenaTrabajador'),
-                'eliminado' => '0'
-            );
-        } elseif($tipoRegistro == 2){
-            $data['datos'] = array(
-                'cedula' => $this->input->post('registro_cedulaEmpresa'),
-                'nombre' => $this->input->post('registro_nombreEmpresa'),
-                'nombreFantasia' => $this->input->post('registro_nombreFantasiaEmpresa'),
-                'idMoneda' => 1
-            );
-            $data['direccion'] = array(
-                'pais' => $this->input->post('registro_paisEmpresa'),
-                'provincia' => $this->input->post('registro_provinciaEmpresa'),
-                'canton' => $this->input->post('registro_cantonEmpresa'),
-                'domicilio' => $this->input->post('registro_domicilioEmpresa')
-            );
-            $data['usuario'] = array(
-                'primerApellido' => $this->input->post('registro_empresaPrimerApellidoContacto'),
-                'segundoApellido' => $this->input->post('registro_empresaSegundoApellidoContacto'),
-                'nombre' => $this->input->post('registro_empresaNombreContacto'),
-                'correo' => $this->input->post('registro_empresaCorreoContacto'),
-                'contrasena' => $this->input->post('registro_empresaContrasenaContacto'),
-                'eliminado' => '0'
-            );
-        }
-        $resultado = $this->Registro_model->registrar($data);
-        if (!$resultado) {
-            //Error en la transacción
-            echo 0;
+        $captcha = $this->rpHash($this->input->post('defaultReal'));
+        $valor = $this->input->post('defaultRealHash');
+        if($captcha == $valor) {
+            $tipoRegistro = $this->input->post('registro_tipo');
+            $data = array();
+            if ($tipoRegistro == 1) {
+                $data['datos'] = array(
+                    'cedula' => $this->input->post('registro_cedulaTrabajador'),
+                    'nombre' => $this->input->post('registro_nombreEmpresaTrabajador'),
+                    'idMoneda' => 1
+                );
+                $data['direccion'] = array(
+                    'pais' => $this->input->post('registro_paisTrabajador'),
+                    'provincia' => $this->input->post('registro_provinciaTrabajador'),
+                    'canton' => $this->input->post('registro_cantonTrabajador'),
+                    'domicilio' => $this->input->post('registro_domicilioTrabajador')
+                );
+                $data['usuario'] = array(
+                    'primerApellido' => $this->input->post('registro_primerApellidoTrabajador'),
+                    'segundoApellido' => $this->input->post('registro_segundoApellidoTrabajador'),
+                    'nombre' => $this->input->post('registro_nombreTrabajador'),
+                    'correo' => $this->input->post('registro_correoTrabajador'),
+                    'contrasena' => $this->input->post('registro_contrasenaTrabajador'),
+                    'eliminado' => '0'
+                );
+            } elseif ($tipoRegistro == 2) {
+                $data['datos'] = array(
+                    'cedula' => $this->input->post('registro_cedulaEmpresa'),
+                    'nombre' => $this->input->post('registro_nombreEmpresa'),
+                    'nombreFantasia' => $this->input->post('registro_nombreFantasiaEmpresa'),
+                    'idMoneda' => 1
+                );
+                $data['direccion'] = array(
+                    'pais' => $this->input->post('registro_paisEmpresa'),
+                    'provincia' => $this->input->post('registro_provinciaEmpresa'),
+                    'canton' => $this->input->post('registro_cantonEmpresa'),
+                    'domicilio' => $this->input->post('registro_domicilioEmpresa')
+                );
+                $data['usuario'] = array(
+                    'primerApellido' => $this->input->post('registro_empresaPrimerApellidoContacto'),
+                    'segundoApellido' => $this->input->post('registro_empresaSegundoApellidoContacto'),
+                    'nombre' => $this->input->post('registro_empresaNombreContacto'),
+                    'correo' => $this->input->post('registro_empresaCorreoContacto'),
+                    'contrasena' => $this->input->post('registro_empresaContrasenaContacto'),
+                    'eliminado' => '0'
+                );
+            }
+            $resultado = $this->Registro_model->registrar($data);
+            if (!$resultado) {
+                //Error en la transacción
+                echo 0;
+            } else {
+                //Exito en la transaccion
+                echo 1;
+//            redirect('inicio/index');
+            }
         } else {
-            //Exito en la transaccion
-//            echo 1;
-            redirect('inicio/index');
+            //Error en el captcha
+            echo '2 , El valor es '.$valor;
         }
+    }
+
+    function rpHash($value) {
+        $hash = 5381;
+        $value = strtoupper($value);
+        for($i = 0; $i < strlen($value); $i++) {
+            $hash = (($hash << 5) + $hash) + ord(substr($value, $i));
+        }
+        return $hash;
     }
 
 }
