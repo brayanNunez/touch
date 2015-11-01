b5df9dfdf951527c092a15fbcda9c6b8898cea62
brayanNunez <brayannr22@gmail.com>
2015-11-01 14:12:19 -0600

    ver /editar

--- application/views/clientes/clientes_lista.php
+++ application/views/clientes/clientes_lista.php
@@ -101,11 +101,11 @@
                                                            <ul id="dropdown-cliente<?= $contador ?>"
                                                               class="dropdown-content">
                                                               <li>
-                                                                 <a href="<?= base_url(); ?>clientes/editar"
+                                                                 <a href="<?= base_url(); ?>clientes/editar/<?= $idEncriptado?>"
                                                                     class="-text"><?= label('menuOpciones_ver') ?></a>
                                                              </li>
                                                               <li>
-                                                                 <a href="<?= base_url() ?>clientes/editar/<?= $idEncriptado ?>"
+                                                                 <a href="<?= base_url() ?>clientes/editar/<?= $idEncriptado?>/#tab-edicion"
                                                                     class="-text"><?= label('menuOpciones_editar') ?></a>
                                                               </li>
                                                               <li>
